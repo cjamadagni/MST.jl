56dc23a8679c2e55630dee9a147f6cf1c664ace3
cjamadagni <chirag.jamadagni@gmail.com>
2015-12-17 00:31:45 +0530

    Initial Commit

---
+++ mst.jl
@@ -0,0 +1,37 @@
+using LightGraphs
+
+
+function minimum_spanning_tree(distmx, n)
+
+	edgeList = []
+	edgeCount = 0
+
+	mst = Graph(n)
+
+	for i in 1:n
+		for j in i+1:n
+			push!(edgeList,(distmx[i,j],i,j))	
+		end
+	end
+
+	
+	for i in 1:length(edgeList)
+		
+		add_edge!(mst,edgeList[i][2],edgeList[i][3])
+		edgeCount=edgeCount + 1
+           
+           if is_cyclic(mst)
+               rem_edge!(mst,edgeList[i][2],edgeList[i][3])
+               edgeCount = edgeCount - 1
+           end
+           
+           if edgeCount == (n-1)
+               break
+           end
+    end
+
+    return mst
+
+end
+
+
